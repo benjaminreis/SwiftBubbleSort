5d5264f6582d0348023d271b98384eb076e835e6
benjaminreis <benjaminreis@Bens-MacBook-Pro.local>
2015-06-21 21:04:07 -0500

    finished project

--- BubbleSort/main.swift
+++ BubbleSort/main.swift
@@ -8,5 +8,31 @@
 
 import Foundation
 
-println("Hello, World!")
+var array: [Int] = [99, 88, 56, 3, 1, 91, 43, 11, 53]
 
+func swapNum(indexOne :Int, indexTwo :Int) {
+    let tempNum = array[indexOne]
+    
+    array[indexOne] = array[indexTwo]
+    array[indexTwo] = tempNum
+}
+
+    var flip = true;
+
+    while(flip) {
+        flip = false
+        for var i = 0; i < array.count-1; ++i{
+            
+            if array[i] > array[i+1] {
+                flip = true
+                swapNum(i, i+1)
+            }
+        }
+
+    }
+
+
+for item in array {
+    print(item)
+    print("\n")
+}
